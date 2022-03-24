30ca6e23bc8e21b20a2114b8617e27df9bcef0fd
Unam3dd <skyrim.gaming16@gmail.com>
2022-03-24 20:40:41 +0100

    update

--- shit/hello_world/hello_world.s
+++ shit/hello_world/hello_world.s
@@ -1,7 +1,12 @@
-BITS 64
+; Title     : Hello world
+; Assembly  : NASM
+; Target    : Linux - x86_64
+; Author    : S4M0verfl0w
+; Created   : 24/03/2022 20:39:10
+; Compile with ld  : nasm -f elf64 hello_world.s && ld hello_world.o -o hello
+; Compile with gcc : nasm -f elf64 hello_world.s && gcc hello_world.o -nostartfiles --entry=_start -o hello
 
-; Compile : nasm -f elf64 hello_world.s && ld hello_world.o -o hello
-; Or Compile with gcc : nasm -f elf64 hello_world.s && gcc hello_world.o -nostartfiles --entry=_start -o hello
+BITS 64
 
 section .data
     msg db "hello", 0
