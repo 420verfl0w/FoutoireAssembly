986f0c44b400fe5e6f965d16469586e8eead5061
Pixailz <Pixailz1@gmail.com>
2022-03-25 15:33:56 +0100

    :fire: libasm/div.s: remove unecessary comment

--- shit/libasm/div.s
+++ shit/libasm/div.s
@@ -3,7 +3,7 @@ BITS 64
 div_nbr:
 	mov		rax, rdx	; store dividend on rax
 	xor		rdx, rdx	; clear rdx or SIGFPE appears
-	div		rbx			; store divisor on rbx
+	div		rbx
 	ret
 
 	; rdx will contains the remainder
