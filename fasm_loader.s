1ba6815f3b544077957071720b0a871bbe0a10da
sam0verfl0w <34691959+Unam3dd@users.noreply.github.com>
2022-04-23 20:43:09 +0200

    Add files via upload

--- fasm_loader.s
+++ fasm_loader.s
@@ -4,16 +4,20 @@ include 'elf.inc'
 
 segment readable writable
     virtual at rsp
-        my elf_ehdr 2
+        my mytest
+        szmy = $ - rsp
     end virtual
+
 
 segment readable executable
 
 entry _start
 
 _start:
+    sub rsp, szmy
     mov byte [my.a], 0x1
     mov byte [my.b], 0x2
+    add rsp, szmy
     
 _exit:
     mov rax, 0x3c
